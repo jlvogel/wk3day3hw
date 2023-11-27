2a9434b4ee8d8f3c0212b625bedaf72d1d0a2ee1
Joshua Vogel <40187940+jlvogel@users.noreply.github.com>
2023-11-27 00:59:46 -0500

    Initial commit with problem statement and empty Governor() class.

---
+++ script.js
@@ -0,0 +1,9 @@
+// Static Methods and Properties
+
+// There is only one Governor in the state, andd static methods and properties you'd expect a Governor to have.
+
+// log the properties and test the methods (don't instantiate)
+
+class Governor {
+  
+}

65670d76c6daad15329e362445e4e667c68a546c
Joshua Vogel <40187940+jlvogel@users.noreply.github.com>
2023-11-27 01:31:15 -0500

    thinking more about static properties and methods for a Governor class so a lot of comments

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 // Static Methods and Properties
 
-// There is only one Governor in the state, andd static methods and properties you'd expect a Governor to have.
+// There is only one Governor in the state, add static methods and properties you'd expect a Governor to have.
 
 // log the properties and test the methods (don't instantiate)
 
+
+// Thinking about a governor's properties:
+// the particular state the governor is in.
+// the political party the governor is of.
+
+// Thinking about a governor's methods:
+// signing legislation
+// vetoing legislation
+// preparing state budget
+// administernig state budget
+// implementing state laws
+
+// Ok, but after some research on static properties and methods,
+// these are properties and methods that are particular to the class
+// itself, not on any instance.  static properties and methods, are
+// not available to instances of the class.
+
+// So we need to think about what is particular to the class Governor as a whole, that wouldn't be applicable to a particular governor of a particular state.  
+
+// hmmmmmmmmmmmmmmmm
+
+
+
 class Governor {
-  
+
 }
